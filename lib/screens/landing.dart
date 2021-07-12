8d61af531f311094ff17afa8616bd6e3a7d66507
danyu rajbahak <rajbahakdanyu@gmail.com>
2021-07-12 21:11:54 +0545

    Finished transaction item

--- lib/screens/landing.dart
+++ lib/screens/landing.dart
@@ -1,4 +1,6 @@
+import 'package:avatars/avatars.dart';
 import 'package:flutter/material.dart';
+import 'package:styled_text/styled_text.dart';
 
 import '../components/top_nav.dart';
 import '../components/bottom_nav.dart';
@@ -51,10 +53,48 @@ class _LandingPageState extends State<LandingPage> {
                     horizontal: 20.0,
                   ),
                   child: Container(
-                    height: 50,
+                    height: 80,
                     width: double.infinity,
                     child: Row(
-                      children: [],
+                      children: [
+                        Avatar(
+                          name: 'Bill Gate',
+                          shape: AvatarShape.rectangle(
+                            50,
+                            50,
+                            BorderRadius.all(new Radius.circular(5.0)),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: StyledText(
+                            text:
+                                '<black>Bill Gates</black> \n<grey>12 July, 2021</grey>',
+                            tags: {
+                              'black': StyledTextTag(
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 22,
+                                ),
+                              ),
+                              'grey': StyledTextTag(
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                ),
+                              ),
+                            },
+                          ),
+                        ),
+                        SizedBox(width: 100),
+                        Text(
+                          '\$145.00',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                 ),
