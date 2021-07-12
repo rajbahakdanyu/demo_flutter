0c0eeb77fca72c3b518ccf76353153fefc815be7
danyu rajbahak <rajbahakdanyu@gmail.com>
2021-07-12 14:19:55 +0545

    Adde a landing page

---
+++ lib/screens/landing.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class LandingPage extends StatefulWidget {
+  const LandingPage({Key? key}) : super(key: key);
+
+  @override
+  _LandingPageState createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Landing Page'),
+      ),
+    );
+  }
+}
