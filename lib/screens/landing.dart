8e0529012e730525b3683d1eadf752a7bbb0ffc4
danyu rajbahak <rajbahakdanyu@gmail.com>
2021-07-12 15:36:09 +0545

    Updated bottom navigation items

--- lib/screens/landing.dart
+++ lib/screens/landing.dart
@@ -14,6 +14,40 @@ class _LandingPageState extends State<LandingPage> {
       body: Center(
         child: Text('Landing Page'),
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.grey[200],
+        selectedItemColor: Colors.blueAccent,
+        unselectedItemColor: Colors.black,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        iconSize: 30,
+        currentIndex: 0,
+        onTap: (int i) {
+          print(i);
+        },
+        items: [
+          BottomNavigationBarItem(
+            label: 'Home',
+            icon: Icon(Icons.home_outlined),
+            activeIcon: Icon(Icons.home_filled),
+          ),
+          BottomNavigationBarItem(
+            label: 'Pie Chart',
+            icon: Icon(Icons.pie_chart_outline),
+            activeIcon: Icon(Icons.pie_chart),
+          ),
+          BottomNavigationBarItem(
+            label: 'Line Chart',
+            icon: Icon(Icons.bar_chart_outlined),
+            activeIcon: Icon(Icons.bar_chart),
+          ),
+          BottomNavigationBarItem(
+            label: 'Profile',
+            icon: Icon(Icons.person_outline),
+            activeIcon: Icon(Icons.person),
+          ),
+        ],
+      ),
     );
   }
 }
