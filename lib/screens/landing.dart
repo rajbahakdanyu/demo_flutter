fbf129d00c0ef0ec5ab2c7f934be2a88aa11ab38
danyu rajbahak <rajbahakdanyu@gmail.com>
2021-07-12 15:51:30 +0545

    Made bottom navigation bar a component

--- lib/screens/landing.dart
+++ lib/screens/landing.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../components/bottom_nav.dart';
+
 class LandingPage extends StatefulWidget {
   const LandingPage({Key? key}) : super(key: key);
 
@@ -19,41 +21,7 @@ class _LandingPageState extends State<LandingPage> {
           topLeft: Radius.circular(35.0),
           topRight: Radius.circular(35.0),
         ),
-        child: BottomNavigationBar(
-          type: BottomNavigationBarType.fixed,
-          backgroundColor: Colors.grey[200],
-          selectedItemColor: Colors.blueAccent,
-          unselectedItemColor: Colors.black,
-          showSelectedLabels: false,
-          showUnselectedLabels: false,
-          iconSize: 30,
-          currentIndex: 0,
-          onTap: (int i) {
-            print(i);
-          },
-          items: [
-            BottomNavigationBarItem(
-              label: 'Home',
-              icon: Icon(Icons.home_outlined),
-              activeIcon: Icon(Icons.home_filled),
-            ),
-            BottomNavigationBarItem(
-              label: 'Pie Chart',
-              icon: Icon(Icons.pie_chart_outline),
-              activeIcon: Icon(Icons.pie_chart),
-            ),
-            BottomNavigationBarItem(
-              label: 'Line Chart',
-              icon: Icon(Icons.bar_chart_outlined),
-              activeIcon: Icon(Icons.bar_chart),
-            ),
-            BottomNavigationBarItem(
-              label: 'Profile',
-              icon: Icon(Icons.person_outline),
-              activeIcon: Icon(Icons.person),
-            ),
-          ],
-        ),
+        child: BottomNav(),
       ),
     );
   }
