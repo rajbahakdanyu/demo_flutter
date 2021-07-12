10a676264a3e241507815a97a133ae4b754dd02b
danyu rajbahak <rajbahakdanyu@gmail.com>
2021-07-12 16:02:58 +0545

    Added profile page to routes

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import './screens/home.dart';
 import './screens/landing.dart';
+import './screens/profile.dart';
 
 void main() {
   runApp(MyApp());
@@ -20,6 +21,7 @@ class MyApp extends StatelessWidget {
       routes: {
         '/': (context) => HomePage(),
         'landing': (context) => LandingPage(),
+        'profile': (context) => ProfilePage(),
       },
     );
   }
