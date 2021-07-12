5281a393ebd5e0e678c8c0d583415445b7a13553
danyu rajbahak <rajbahakdanyu@gmail.com>
2021-07-12 15:47:24 +0545

    Added rounded choners to bottom navigation bar

--- lib/screens/landing.dart
+++ lib/screens/landing.dart
@@ -14,40 +14,46 @@ class _LandingPageState extends State<LandingPage> {
       body: Center(
         child: Text('Landing Page'),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        backgroundColor: Colors.grey[200],
-        selectedItemColor: Colors.blueAccent,
-        unselectedItemColor: Colors.black,
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        iconSize: 30,
-        currentIndex: 0,
-        onTap: (int i) {
-          print(i);
-        },
-        items: [
-          BottomNavigationBarItem(
-            label: 'Home',
-            icon: Icon(Icons.home_outlined),
-            activeIcon: Icon(Icons.home_filled),
-          ),
-          BottomNavigationBarItem(
-            label: 'Pie Chart',
-            icon: Icon(Icons.pie_chart_outline),
-            activeIcon: Icon(Icons.pie_chart),
-          ),
-          BottomNavigationBarItem(
-            label: 'Line Chart',
-            icon: Icon(Icons.bar_chart_outlined),
-            activeIcon: Icon(Icons.bar_chart),
-          ),
-          BottomNavigationBarItem(
-            label: 'Profile',
-            icon: Icon(Icons.person_outline),
-            activeIcon: Icon(Icons.person),
-          ),
-        ],
+      bottomNavigationBar: ClipRRect(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(35.0),
+          topRight: Radius.circular(35.0),
+        ),
+        child: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: Colors.grey[200],
+          selectedItemColor: Colors.blueAccent,
+          unselectedItemColor: Colors.black,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          iconSize: 30,
+          currentIndex: 0,
+          onTap: (int i) {
+            print(i);
+          },
+          items: [
+            BottomNavigationBarItem(
+              label: 'Home',
+              icon: Icon(Icons.home_outlined),
+              activeIcon: Icon(Icons.home_filled),
+            ),
+            BottomNavigationBarItem(
+              label: 'Pie Chart',
+              icon: Icon(Icons.pie_chart_outline),
+              activeIcon: Icon(Icons.pie_chart),
+            ),
+            BottomNavigationBarItem(
+              label: 'Line Chart',
+              icon: Icon(Icons.bar_chart_outlined),
+              activeIcon: Icon(Icons.bar_chart),
+            ),
+            BottomNavigationBarItem(
+              label: 'Profile',
+              icon: Icon(Icons.person_outline),
+              activeIcon: Icon(Icons.person),
+            ),
+          ],
+        ),
       ),
     );
   }
