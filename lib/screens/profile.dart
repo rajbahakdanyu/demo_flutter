518cc8b220dbbafa005bbe29056718fac66a7474
danyu rajbahak <rajbahakdanyu@gmail.com>
2021-07-12 22:54:02 +0545

    Added profile row

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -54,6 +54,50 @@ class _ProfilePageState extends State<ProfilePage> {
             ),
           ),
           ExpenseList(),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 20.0,
+              horizontal: 20.0,
+            ),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Text(
+                    'Income',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: Text(
+                    'Expenses',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 0,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.yellow[700],
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: IconButton(
+                      color: Colors.white,
+                      onPressed: () => {},
+                      icon: Icon(Icons.calendar_today_outlined),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
       bottomNavigationBar: ClipRRect(
