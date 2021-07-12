e4f733c0cd0ce7a9f84ca770b49affcb9d01f23d
danyu rajbahak <rajbahakdanyu@gmail.com>
2021-07-12 19:17:14 +0545

    Created refer card component

---
+++ lib/components/refer_card.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ReferCard extends StatelessWidget {
+  const ReferCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
